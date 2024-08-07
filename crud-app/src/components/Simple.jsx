1321c8be5b8723f20502081d151145a8e27afc26
Edwin Jaret <utp0149483@alumno.utpuebla.edu.mx>
2024-08-07 09:53:45 -0600

    Modificar porque falla

--- crud-app/src/components/Simple.jsx
+++ crud-app/src/components/Simple.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Simple = ({ titulo, subtitulo, nombre }) => {
     return (
         <>
-            <h1>{titulo}</h1>
+            <h1 data-testid={'test-title'}>{titulo}</h1>
             <h4>{subtitulo}</h4>
             <p>{nombre}</p>
         </>
--- crud-app/test/components/Simple.test.jsx
+++ crud-app/test/components/Simple.test.jsx
@@ -1,16 +1,17 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Simple from "../../src/components/Simple";
 
 describe('Pruebas generales del componente Simple', () => {
+    const titulo = "Hello Word"
     test('Se realiza comparacion con snapshot', () => {
         const { container } = render(<Simple titulo='Hello Word'
-            subtitulo="Final de Cuatri" />);
+            subtitulo="Final de Cuatri" nombre={"Jose"} />);
         console.log(container);
         expect(container).toMatchSnapshot();
     });
 
     test('Se debe mostrar el titulo dentro de un h1', () => {
-        const titulo = "Hola Mundo"
+        
         const { container, getByText } = render(<Simple titulo={
             titulo} />)
         expect(getByText(titulo)).toBeTruthy();
@@ -18,4 +19,24 @@ describe('Pruebas generales del componente Simple', () => {
         const h1 = container.querySelector("h1")
         expect(h1.innerHTML).toContain(titulo);
     });
+
+    test('Verifica si el elemento test-id contiene el titulo', () => {
+        const { getByTestId } = render(<Simple titulo={titulo} />);
+        //expect (getByTextId('test-title')).toBeTruthy()
+        expect(getByTestId('test-title').innerHTML).toContain(titulo)
+
+    });
+
+    test('El numero de elementos coincide ', ()=>{
+        const subtitulo = "Hola desde Reack";
+        const {getByText, getAllByText} = render(<Simple titulo={titulo}
+        subtitulo={subtitulo}/>);
+        //expect (getByText(subtitulo)).toBeTruthy()
+        expect(getAllByText(subtitulo).length).toBe(2);
+    });
+
+    test('Debe mostrar el mensaje hola mundo', () =>{
+        screen.debug();
+        expect (screen.getByText(titulo)).toBeTruthy();
+    }) 
 });
